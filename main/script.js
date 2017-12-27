6db6314d84185dd48fd3c555adb8e158ecb62692
Ross Nkama <ross.nkama@adacollege.org.uk>
2017-12-27 16:39:05 +0000

    Scrollspy added

--- main/script.js
+++ main/script.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-    $('.scrollspy').scrollSpy();
-});
+offset = 50;
+
+// onClick event for all navbar links("a")
+$(".nav a").on("click", function (event) {
+    
+    // Making sure that the navbar's link has a hash value
+    if (this.hash !== "") {
+        
+        var hash = this.hash;
+        
+        // Animation
+        $("html").animate({
+
+            // Scroll to said hash
+            scrollTop: $(hash).offset().top - offset
+
+        // Animation lasts 800ms 
+        }, 800, function() {
+
+            // Adding has to the page's URL
+            window.location.hash = hash;
+        });
+    }
+})
